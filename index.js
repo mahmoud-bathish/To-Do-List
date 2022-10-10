b5ddeefa1967112ecfecb55ee2a986f5f7a0ab06
unknown <mahmoudbathish23@gmail.com>
2022-10-11 01:40:12 +0300

    Add LocalStorage

--- index.js
+++ index.js
@@ -1,82 +1,130 @@
 let theInput = document.querySelector(".add-task input");
 let theAddButton = document.querySelector(".add-task .plus");
 let tasksContainer = document.querySelector(".tasks-content");
+let tasksContainer2 = document.querySelector(".tasks-content .task-box ");
 let tasksCount = document.querySelector(".task-count span");
 let taskscompleted = document.querySelector(".tasks-completed span");
+console.log(tasksContainer)
 
 //Focus on input field
-window.onload = function(){
+window.onload = function () {
     theInput.focus();
 }
 
+let arrayOfTasks = [];
+
+if (localStorage.getItem("tasks")) {
+    arrayOfTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+getFromLocalStorage();
+
 //Adding the task
-theAddButton.onclick = function(){
+theAddButton.onclick = function () {
     //if input is empty
-    if (theInput.value == '') {
-        console.log("No Value");
-    } else {
+    if (theInput.value !== '') {
         let noTasksMsg = document.querySelector(".no-tasks-message");
         if (document.body.contains(document.querySelector(".no-tasks-message"))) {
             noTasksMsg.remove();
         }
-        //create span ele
-        let mainSpan = document.createElement("span");
-        //create delte button
-        let deleteElement = document.createElement("span");
-        //create the span text 
-        let text = document.createTextNode(theInput.value);
-        //Create the Delete button 
-        let deleteText = document.createTextNode("Delete");
-        //add text to span 
-        mainSpan.appendChild(text);
-        //add class to span
-        mainSpan.className = 'task-box';
-        //add text to delete
-        deleteElement.appendChild(deleteText);
-        //add class to delete button
-        deleteElement.className = 'delete';
-        //add delete button to main span
-        mainSpan.appendChild(deleteElement);
-        //add the task to container
-        tasksContainer.appendChild(mainSpan);
-        theInput.value = '';
+
+        addTaskToArray(theInput.value);
+        theInput.value = "";
         theInput.focus();
     }
 }
 
-document.addEventListener('click',function(e){
-    if (e.target.className=='delete') {
-        e.target.parentNode.remove();
-        // calculateTasks()
-        if(tasksContainer.childElementCount == 0){
+document.addEventListener('click', function (e) {
+    if (e.target.className == 'delete') {
+        if (tasksContainer.childElementCount == 0) {
             createNoTasks();
         }
+        deleteTaskWith(e.target.parentElement.getAttribute("data-id"));
+        e.target.parentNode.remove();
     }
     if (e.target.classList.contains('task-box')) {
-        e.target.classList.toggle("finished");
-        // calculateTasks()
+        e.target.classList.toggle("tasks-completed");
+        toggleStatus(e.target.getAttribute("data-id"));
     }
-    
-
 })
 
-// document.addEventListener('click',function(e){
-//     if (e.target.className == 'remove-all') {
-//            document.querySelector(".task-box").innerHTML = '';
-//     }
-
 // })
-function createNoTasks() {
-    let msgSpan = document.createElement("span");
-    let msgText = document.createTextNode("No Tasks To show");
-    msgSpan.appendChild(msgText);
-    msgSpan.className = 'no-tasks-message';
-    tasksContainer.appendChild(msgSpan);
+// function createNoTasks() {
+//     let msgSpan = document.createElement("span");
+//     let msgText = document.createTextNode("No Tasks To show");
+//     msgSpan.appendChild(msgText);
+//     msgSpan.className = 'no-tasks-message';
+//     tasksContainer.appendChild(msgSpan);
+// }
+function addTaskToArray(taskText) {
+    const task = {
+        id: Date.now(),
+        title: taskText,
+        completed: false,
+    }
+
+    arrayOfTasks.push(task);
+    addEleToPageFrom(arrayOfTasks);
+    //Local Storage
+    addToLocalStorage(arrayOfTasks);
 }
 
-function calculateTasks() {
-    tasksContainer.innerHTML = document.querySelectorAll('.tasks-content .tasks-box').length;
-    taskscompleted.innerHTML = document.querySelectorAll('.tasks-content .finished').length;
+function addEleToPageFrom(arrayOfTasks) {
+    tasksContainer.innerHTML = "";
+    arrayOfTasks.forEach((task) => {
+        let div = document.createElement("div");
+        div.className = 'task-box';
+        if (task.completed) {
+            div.className = "task-box";
+        }
+        div.setAttribute("data-id", task.id);
+        div.appendChild(document.createTextNode(task.title));
+        let deleteElement = document.createElement("span");
+        deleteElement.className = 'delete';
+        deleteElement.appendChild(document.createTextNode("Delete"));
+        div.appendChild(deleteElement);
+        tasksContainer.appendChild(div);
+        tasksCount.innerHTML = arrayOfTasks.length;
+        let a = arrayOfTasks.filter((task) => task.completed = true);
+        // a.forEach(e =>{e.classList.add("tasks-completed")})
+        
+        taskscompleted.innerHTML = a.length;
+    });
+}
+
+function addToLocalStorage(arrayOfTasks) {
+    window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+}
+
+function getFromLocalStorage() {
+    let data = window.localStorage.getItem("tasks");
+    if (data) {
+        let tasks = JSON.parse(data);
+        addEleToPageFrom(tasks);
+    }
+}
+
+function deleteTaskWith(taskId) {
+    for (let i = 0; i < arrayOfTasks.length; i++) {
+        arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId)
+        addToLocalStorage(arrayOfTasks);
+    }
+    tasksCount.innerHTML = arrayOfTasks.length;
+
+}
+
+function toggleStatus(taskId) {
+    for (let i = 0; i < arrayOfTasks.length; i++) {
+        if (arrayOfTasks[i].id == taskId) {
+           if (arrayOfTasks[i].completed == false){
+            arrayOfTasks[i].completed = true ;
+            taskscompleted.innerHTML ++;
+           } else{
+            arrayOfTasks[i].completed = false
+           } 
+        }
+    }
+    addToLocalStorage(arrayOfTasks);
 }
 
 
